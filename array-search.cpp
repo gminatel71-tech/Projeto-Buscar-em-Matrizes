59ec2b1db66202e99148d4054115fe44102fb566
Gabriel Minatel <gabriel.minatel71@gmail.com>
2026-06-30 09:58:30 -0300

    metodo para organizar matrizes 2D

--- array-search.cpp
+++ array-search.cpp
@@ -31,12 +31,13 @@ int main() {
     cout << endl;
 
     // Busca tradicional (Passar por cada valor da matriz) 
+    /*
     int x;
     bool encontrado;
     encontrado = false;
     cout << "Qual valor você quer buscar na matriz: ";
     cin >> x;
-
+    
     for(i = 0; i < n; i++) {
         for(j = 0; j < m; j++) {
             if(arr[i][j] == x) {
@@ -48,6 +49,69 @@ int main() {
     if(encontrado != true) {
         cout << "Valor: "<< x << " não exite na matriz" << endl;
     }
+    */
+
+    // Para não ocorrer x², preciso tornar o algortimo de pesquisa em log(linha) + log(coluna) da matrix
+    // Sistema de busca binária em uma matriz n x n ou matriz n x m
+   
+    // Organizar a matriz
+
+    int x;
+    int linha = n;
+    int coluna = m;
+    int tamanho = linha * coluna;
+    // deixar a matriz 2D plana (1D)
+    int vet[tamanho];
+    int index = 0;
+    for(i = 0; i < linha; i++) {
+        for(j = 0; j < coluna; j++) {
+            vet[index] = arr[i][j];
+            index++;
+        }
+    } 
+    // método de inserção em vetor
+    for(i = 1; i < tamanho; i++) {
+        int chave = vet[i];
+        j = i - 1;
+
+        while((j >= 0) && (vet[j] > chave)) {
+            vet[j + 1] = vet[j];
+            j--;
+        }
+        vet[j + 1] = chave;  
+    }
+    // Colocar os valores novamente na matriz 2D 
+    index = 0;
+    for(i = 0; i < linha; i++) {
+        for(j = 0; j < coluna; j++) {
+            arr[i][j] = vet[index];
+            index++;
+        }
+    }
+    cout << endl;
+    cout << "MATRIZ COM VALORES ORGANIZADOS: " << endl;
+    for(i = 0; i < linha; i++) {
+        for(j = 0; j < coluna; j++) {
+            cout << arr[i][j] << "   ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    cout << "Preparada para busca binária" << endl;
+/*
+bool encontrado;
+encontrado = false;
+cout << "Qual valor você quer buscar na matriz: ";
+cin >> x;
+
+int inicio = 0;
+int fim = linha * coluna - 1;
+int meio = inicio + (fim - inicio) / 2;
+
+while(inicio <= fim) {
+
+} 
+*/
 
     return 0;
 }
